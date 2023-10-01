d62469800dcdbdf6c987dc91eee02bfb8a2faacf
Subhajit Mondal <mondalsubhajit204@gmail.com>
2023-10-01 13:59:19 +0530

    Added 9 C programmes

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+	char ch;
+	printf ("Enter the letter : ");
+	scanf("%c",&ch);
+	if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+	printf("Vowel");
+	}
+	else{
+	printf("Consonant");
+}
+}
