dbdd1b0864e259cb6462822ae9bc759a7f1b0fcd
Sagar Jaglan <sagarjaglan0511@gmail.com>
2023-10-04 23:25:17 +0530

    added number of good pairs code

---
+++ number_good_pair.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int num_identical_pairs(vector<int>& nums) {
+    int count = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] == nums[j]) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    cout << "Number of good pairs: " << num_identical_pairs(nums) << endl;
+    return 0;
+}
