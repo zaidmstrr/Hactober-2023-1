0c24bf7963c5062b07197df16e8e97210ff52921
Manya0209 <98685510+Manya0209@users.noreply.github.com>
2023-10-07 01:39:21 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,20 +2,22 @@
 
 using namespace std;
 
-int binarySearch(int arr[], int n, int key) {
-  int s = 0;
-  int e = n;
-  while (s <= e) {
-    int mid = (s + e) / 2;
-    if (arr[mid] == key) {
-      return mid;
-    } else if (arr[mid] > key) {
-      e = mid - 1;
-    } else {
-      s = mid + 1;
+int binarySearch(int arr[], int n, int s, int e, int key) {
+   //base case
+    if(s>e)
+    return false;
+
+    int mid = s+ (e-s)/2;
+
+    if (arr[mid] == k)
+    return true;
+
+    if(arr[mid]>k){
+        binarySearch(arr, s, mid-1, k);
     }
-  }
-  return -1;
+    else{
+        binarySearch(arr, mid+1, e, k);
+    }
 }
 
 int main() {
