e24c606099c1d6ff222803c9007aa7ed2f408a2f
Anshul shrivas <128448038+anshul-132002@users.noreply.github.com>
2023-10-07 09:57:04 +0530

    BinarySearch Added

---
+++ Binary__Search.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <vector>
+
+int binarySearch(const std::vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[mid] > target) {
+            right = mid - 1;
+        }
+
+        else {
+            left = mid + 1;
+        }
+    }
+
+    return -1; 
+}
+
+int main() {
+    std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 6;
+    
+    int result = binarySearch(arr, target);
+
+    if (result != -1) {
+        std::cout << "Element found at index " << result << std::endl;
+    } else {
+        std::cout << "Element not found in the array." << std::endl;
+    }
+
+    return 0;
+}
