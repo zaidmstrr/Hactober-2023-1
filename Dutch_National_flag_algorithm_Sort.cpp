cfb957f5f3c5b2949b5b28c7f2fbd3bcdaed0c9b
Wafiya <wafiya45mulla@gmail.com>
2023-10-10 09:34:19 +0530

    Added a new cpp file Dutch_National_flag_algorithm_Sort.cpp

---
+++ Dutch_National_flag_algorithm_Sort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main()
+{
+    int n;
+   cout<<"Enter the size of array ";
+   cin>>n;
+   int a[n];
+   //taking input
+   cout<<"Enter the elements ";
+   for(int i=0;i<n;i++)
+   {
+    cin>>a[i];
+   }
+
+//sorting algorithm
+   int low=0,mid=0,high=n-1;
+   while(mid<=high)
+   {
+        if(a[mid]==0)
+        {
+            swap(a[low],a[mid]);
+            low++;
+            mid++;
+        }
+        else if(a[mid]==1)
+        {
+            mid++;
+        }
+        else if(a[mid]==2)
+        {
+            swap(a[mid],a[high]);
+            high--;
+        }
+   }
+   
+   //printing result
+   cout<<"\nFinal sorted array is ";
+   for(int i=0;i<n;i++)
+   {
+    cout<<a[i]<<" ";
+   }
+
+
+}
