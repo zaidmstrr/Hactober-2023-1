c730fc3578c0ab1d06ce4595fec0acf83f22d243
amanadeem <quranrectiation80@gmail.com>
2023-10-23 12:32:11 +0500

    Hackerrank solutions added

---
+++ Hackerrank/Solvemefirst.php
@@ -0,0 +1,14 @@
+<?php
+
+// solveMeFirst has the following parameters:
+
+// int a: the first value
+// int b: the second value
+// Returns
+// - int: the sum of  and 
+
+
+function solveMeFirst($a,$b){
+    // Hint: Type return $a + $b; below  
+    return $a + $b;
+  }
