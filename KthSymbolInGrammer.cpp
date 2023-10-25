54fa4d6866ea4af6edb9bd37576b6d6bb1960000
Nikhil Taneja <60517862+NikhilTaneja98@users.noreply.github.com>
2023-10-25 22:42:20 +0530

    Create KthSymbolInGrammer.cpp
    Added code to find kth symbol in grammer(1-indexed).

---
+++ KthSymbolInGrammer.cpp
@@ -0,0 +1,24 @@
+class KthSymbol {
+public:
+    int kthGrammar(int n, int k) {
+        
+        bool areValuesSame = true; 
+
+        
+        n = pow(2, n - 1);
+
+        
+        while (n != 1) {
+            
+            n /= 2;
+
+            
+            if (k > n) {
+                k -= n;
+                areValuesSame = !areValuesSame;
+            }
+        }
+
+        return ((areValuesSame) ? 0 : 1) ;
+    }
+};
