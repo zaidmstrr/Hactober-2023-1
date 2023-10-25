76402de2e96f7550f8b34891b3a5be157b7bbb06
Pratik Chandra Pal <122289794+Pratik-050@users.noreply.github.com>
2023-10-25 22:30:04 +0530

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(const vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 5;
+
+    int result = binarySearch(arr, target);
+
+    if (result != -1) {
+        cout << "Element " << target << " found at index " << result << endl;
+    } else {
+        cout << "Element " << target << " not found in the array." << endl;
+    }
+
+    return 0;
+}
